3bc367c6ee9239135e9fde0271c91b75a5808bc4
Cosimo Benvenuti <cosimo.benvenuti10@gmail.com>
2026-08-05 16:48:23 -0700

    Add_GameManager

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UI;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private float gameTimerDuration;
+    
+    [Header("References")]
+    [SerializeField] private TimerUI timerUI;
+    
+    public event Action StartGame; 
+    public event Action EndGame;
+    
+    private GameTimer timer;
+
+    private void Awake()
+    {
+        timer = new GameTimer(gameTimerDuration);
+        timer.OnFinish += OnFinishGame;
+        EndGame += timer.Pause;
+        StartGame += timer.Start;
+    }
+    
+    private void Start()
+    {
+        StartGame?.Invoke();
+    }
+
+    private void Update()
+    {
+        timer.Update();
+        timerUI.UpdateTimer(timer.RemainingTime);
+    }
+
+    private void OnFinishGame()
+    {
+        EndGame?.Invoke();
+    }
+}
---
+++ Assets/Scripts/UI/TimerUI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class TimerUI : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text timerText;
+
+        public void UpdateTimer(float time)
+        {
+            timerText.text = Mathf.CeilToInt(time).ToString();
+        }
+    }
+}
